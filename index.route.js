db0f86659d06445fcaddaf1279b09901be09f4e6
Bibek Gurung <bibek.gurung95@gmail.com>
2019-04-18 00:49:54 +0545

    removed user module

--- index.route.js
+++ index.route.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const userRoutes = require('./server/user/user.route');
 const authRoutes = require('./server/auth/auth.route');
 const chatRoutes = require('./server/chat/chat.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
-
-// mount user routes at /users
-router.use('/users', userRoutes);
 
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
